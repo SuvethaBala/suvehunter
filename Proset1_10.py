a7bf3a8504f8548cace5f67a78ee4ab2cc4f455a
SuvethaBala <46593305+SuvethaBala@users.noreply.github.com>
2019-03-30 18:36:28 +0530

    Update Proset1_10.py

--- Proset1_10.py
+++ Proset1_10.py
@@ -2,8 +2,11 @@ n=int(input())
 steps=(list(map(int.input().split))
 sum=0
 for i in range(l,len(steps)):
-for j in range(0,i):
-if (steps[j]< steps[i]):
+       for j in range(0,i):
+       
+       
 
-sum=sum+steps[j]
-print(sum)
+
+
+
+
