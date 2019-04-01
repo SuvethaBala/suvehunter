46b7cf61b48f0cf4139d44cfd5715cd894bfda97
SuvethaBala <46593305+SuvethaBala@users.noreply.github.com>
2019-04-01 22:54:18 +0530

    Create Pro25.py

---
+++ Pro25.py
@@ -0,0 +1,14 @@
+N=int(input())
+a=list(map(int,(input()).split()))
+b= 1
+c = 1
+for i in range(1,N):
+    if (a[i] > a[i-1]):
+        c =c + 1
+    else:
+        if (b < c):
+            b = c
+        c = 1
+if (b < c):
+    b = c
+print(b)
