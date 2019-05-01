27886962b287709da2c5206309359984e21fe4ff
SuvethaBala <46593305+SuvethaBala@users.noreply.github.com>
2019-05-01 22:09:07 +0530

    Create Hunter14.py

---
+++ Hunter14.py
@@ -0,0 +1,11 @@
+from itertools import permutations
+n=list(input())
+p = permutations(n)
+b=[]
+for i in list(p):
+    s=''
+    for j in i:
+       s+=j
+    if s not in b:
+       b.append(s)
+       print(s)
