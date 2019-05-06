50cf514fdc3d8c7b3f783539408e10fd804d2499
SuvethaBala <46593305+SuvethaBala@users.noreply.github.com>
2019-05-06 21:44:44 +0530

    Update Pro27.py

--- Pro27.py
+++ Pro27.py
@@ -1,15 +1,15 @@
 n,W=map(int,input().split())
-wt=list(map(int,input().split()))
+vt=list(map(int,input().split()))
 val=list(map(int,input().split()))
 ratio=[]
 for i in range(n):
-  ratio.append(val[i]/wt[i])
+  ratio.append(val[i]/vt[i])
 cost=0
 while W>=0 and len(ratio)>0:
-  if W>=wt[ratio.index(max(ratio))]:
+  if W>=vt[ratio.index(max(ratio))]:
     cost+=val[ratio.index(max(ratio))]
-    W-=wt[ratio.index(max(ratio))]
+    W-=vt[ratio.index(max(ratio))]
   val.pop(ratio.index(max(ratio)))
-  wt.pop(ratio.index(max(ratio)))
+  vt.pop(ratio.index(max(ratio)))
   ratio.pop(ratio.index(max(ratio)))
 print(cost)
