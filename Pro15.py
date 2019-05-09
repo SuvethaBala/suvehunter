8bf26076100b4007b7aeaa3458c7a635a97089cc
SuvethaBala <46593305+SuvethaBala@users.noreply.github.com>
2019-05-09 22:09:44 +0530

    Update Pro15.py

--- Pro15.py
+++ Pro15.py
@@ -2,10 +2,10 @@ n = int(input())
 in_nums = list(map(int, input().split()))
 
 no_of_ones = []
-for i in in_nums:
+for j in in_nums:
     count = 0
-    while i:
-        i &= (i-1)
+    while j:
+        j &= (j-1)
         count += 1
     no_of_ones.append(count)
 
