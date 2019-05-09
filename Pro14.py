9e0e03cfee60a2e59ff58af71c56f54540dc2b79
SuvethaBala <46593305+SuvethaBala@users.noreply.github.com>
2019-05-09 22:04:16 +0530

    Update Pro14.py

--- Pro14.py
+++ Pro14.py
@@ -8,6 +8,6 @@ q = int(nq[1])
 in_nums = list(map(int, input().split()))
 
 for _ in range(q):
-    u, v = map(int, input().split())
-    x = reduce(xor, in_nums[u-1:v])
+    k, v = map(int, input().split())
+    x = reduce(xor, in_nums[k-1:v])
     print(x)
