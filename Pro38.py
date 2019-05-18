d572bc485dba94c0f4d62b91e10b8f6032c8071a
SuvethaBala <46593305+SuvethaBala@users.noreply.github.com>
2019-05-18 21:38:55 +0530

    Update Pro38.py

--- Pro38.py
+++ Pro38.py
@@ -1,10 +1,10 @@
 n,k=map(int,input().split())
-n1=list(map(int,input().split()))
-n1=sorted(n1)
+n2=list(map(int,input().split()))
+n2=sorted(n2)
 team,i=0,0
 flag=0
-while i<len(n1)-2:
-  a,b,c=n1[i:i+3]
+while i<len(n2)-2:
+  a,b,c=n2[i:i+3]
   for j in range(0,k):
     a,b,c=a+1,b+1,c+1 
     if a<=5 and b<=5 and c<=5:
