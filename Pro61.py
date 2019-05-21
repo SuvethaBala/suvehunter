3675320e4e3b0b687fb6bd1fe589b37eadcf1070
SuvethaBala <46593305+SuvethaBala@users.noreply.github.com>
2019-05-21 22:01:24 +0530

    Update Pro61.py

--- Pro61.py
+++ Pro61.py
@@ -1,11 +1,11 @@
 a = input()
 b = input()
-e = ''
+k = ''
 for i in range(0,len(a)):
   c = ord(a[i])-96
   d = ord(b[i])-96
   if c+d < 26:
-    e = e+chr(ord(chr(c+d+96)))
+    k = k+chr(ord(chr(c+d+96)))
   else:
-    e = e+chr(ord(chr(((c+d)-26)+96)))
-print(e)
+    k = k+chr(ord(chr(((c+d)-26)+96)))
+print(k)
