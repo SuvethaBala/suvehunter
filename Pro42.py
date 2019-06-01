843cfb7aeba30a64e66ad2e7630159fd38b1ecbf
SuvethaBala <46593305+SuvethaBala@users.noreply.github.com>
2019-06-01 22:36:03 +0530

    Update Pro42.py

--- Pro42.py
+++ Pro42.py
@@ -1,17 +1,17 @@
 num,k=map(int,input().split())
-a=list(map(int,input().split()))
+b=list(map(int,input().split()))
 if k==1:
-  print(min(a))
+  print(min(b))
 elif k==2:
-  m=a[0]
-  temp=a[0]
+  m=b[0]
+  temp=b[0]
   for i in range(1,num-1):
-    if min(a[:i])>min(a[i+1:]):
-      temp=min(a[:i])
+    if min(b[:i])>min(b[i+1:]):
+      temp=min(b[:i])
     else:
-      temp=min(a[i+1:])
+      temp=min(n[i+1:])
     if temp>m:
       m=temp
   print(m)
 else:
-  print(max(a))
+  print(max(b))
