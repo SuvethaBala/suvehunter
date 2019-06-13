f8b65fea19322df8508543e03b57acc5e56f641a
SuvethaBala <46593305+SuvethaBala@users.noreply.github.com>
2019-06-13 21:59:50 +0530

    Update Pro52.java

--- Pro52.java
+++ Pro52.java
@@ -8,15 +8,15 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		int a1=sc.nextInt();
+		int c1=sc.nextInt();
 		int b1=sc.nextInt();
-		int a2=sc.nextInt();
+		int c2=sc.nextInt();
 		int b2=sc.nextInt();
-		int a3=sc.nextInt();
+		int c3=sc.nextInt();
 		int b3=sc.nextInt();
-		int a4=sc.nextInt();
+		int c4=sc.nextInt();
 		int b4=sc.nextInt();
-		if(a1==a2&&a3==a4&&b1==b4&&b2==b3)
+		if(c1==c2&&c3==c4&&b1==b4&&b2==b3)
 		{
 			System.out.println("yes");
 		}
