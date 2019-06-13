c9839d766c9c7b5b6ad69ecfe12ac47edb80c834
SuvethaBala <46593305+SuvethaBala@users.noreply.github.com>
2019-06-13 22:17:04 +0530

    Create Pro43.py

---
+++ Pro43.py
@@ -0,0 +1,21 @@
+m = int(input())
+a = list(map(int,input().split()))
+c,l = 0,[]
+b = [x for x in range(1,m+1)]
+for i in a:
+  if i in b:
+    b.remove(i)
+k = 0
+for i in range(0,m-1):#loopstarts
+  p = a[i]
+  for j in range(i+1,m):
+    if p == a[j]:
+      if p < b[k]:
+        a[j] = b[k]
+        c += 1
+      else:
+        a[i] = b[k]
+        c += 1
+      k += 1#loopends
+print(c)
+print(*a)
