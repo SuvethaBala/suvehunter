02329b968d305bcc5d2e48dad38bb4faf8d1a03a
SuvethaBala <46593305+SuvethaBala@users.noreply.github.com>
2019-06-14 22:09:22 +0530

    Update Pro45.py

--- Pro45.py
+++ Pro45.py
@@ -1,13 +1,13 @@
-no=input()
-if no==no[::-1]:
+n=input()
+if n==n[::-1]:
     print("yes")
 else:
-    value=no.strip("0")
+    value=n.strip("0")
     
     if value==value[::-1]:
         print("yes")
     else:
-        value=no.lstrip("0")
+        value=n.lstrip("0")
         
         if value==value[::-1]:
             print("yes")
